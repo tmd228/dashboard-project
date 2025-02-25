031e2d1797df1a4683b15fd98866be2a003edd0a
Raymond Choi <tmd0228@gmail.com>
2025-02-25 11:18:21 +0800

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ChartCard from './components/ChartCard/ChartCard'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import Dashboard from './pages/Dashboard/Dashboard'
+import NotFound from './pages/NotFound/NotFound'
+import Settings from './pages/Settings/Settings'
+
+function App() {
+
+  return (
+   <h1>hello</h1>
+  )
+}
+
+export default App
